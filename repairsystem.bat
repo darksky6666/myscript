342eb6b0ad656696dc4e87cc2e17b2796d314ab3
darksky <84705610+darksky6666@users.noreply.github.com>
2021-12-20 18:30:21 +0800

    Add files via upload

---
+++ repairsystem.bat
@@ -0,0 +1,44 @@
+REM @darksky6666
+@echo off
+title Repair System
+
+REM BatchGotAdmin from Eneerge
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+:--------------------------------------    
+cls
+echo Repairing system file... Please close all the programs...
+echo.
+echo.
+pause
+dism /online /cleanup-image /restorehealth
+sfc /scannow
+echo.
+echo.
+echo Press any key to restart the PC...
+pause >nul
+shutdown -r -t 60
